68735043fe41d52ece23ab24d5561fa7921ba24e
Nguyen Hoang Nam <hoangnam2004hp@gmail.com>
2024-04-21 22:21:02 +0700

    Comment về 2 trường hợp số nguyên

--- TestCarryOut.c
+++ TestCarryOut.c
@@ -3,8 +3,8 @@ int main(){
     short x=32767;
     short y=5;
     short sum=x+y;
-    printf("sum = %d\n", sum);
+    printf("sum = %d\n", sum); //Trường hợp số nguyên có dấu
     unsigned short sum1 =x+y;
-    printf("sum1 = %d\n", sum1);
+    printf("sum1 = %d\n", sum1); // Trường hợp số nguyên không dấu
     return 0;
 }
