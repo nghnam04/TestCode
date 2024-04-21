37858f60ad8a2d59bad705422805ab86b0388273
Nguyen Hoang Nam <hoangnam2004hp@gmail.com>
2024-04-21 22:22:18 +0700

    Comment về khoảng giá trị của kiểu short

--- TestCarryOut.c
+++ TestCarryOut.c
@@ -2,7 +2,7 @@
 int main(){
     short x=32767;
     short y=5;
-    short sum=x+y;
+    short sum=x+y; //Kiểu short được biểu diễn bằng 15 bit nên có khoảng giá trị [-32768; 32767]
     printf("sum = %d\n", sum); //Trường hợp số nguyên có dấu
     unsigned short sum1 =x+y;
     printf("sum1 = %d\n", sum1); // Trường hợp số nguyên không dấu
